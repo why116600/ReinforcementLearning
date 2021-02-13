a3293aee30a7a93bd8d366f961594d4cf7f33333
why116600 <wanghy438@163.com>
2021-02-13 20:41:54 +0800

    CartPole的强化学习

---
+++ CartPoleRL/CartPoleRL.py
@@ -0,0 +1,116 @@
+
+import gym
+import tensorflow as tf
+import numpy as np
+import sys
+import os
+import random
+
+def CreateNewModel(input_len,output_len):
+	model = tf.keras.Sequential([tf.keras.layers.InputLayer(input_shape=(input_len,)),
+		tf.keras.layers.Dense(20,activation='relu'),
+		tf.keras.layers.Dense(20,activation='relu'),
+			tf.keras.layers.Dense(output_len,activation=tf.keras.activations.linear)])
+	model.compile(optimizer=tf.keras.optimizers.Adam(1e-3),loss='mse')
+	return model
+
+BATCH_SIZE=64
+GAMMA=0.95
+
+class DDQN(object):
+	def __init__(self,env):
+		self.nLen=len(env.reset())
+		self.nAction=env.action_space.n
+		self.models=[CreateNewModel(self.nLen,self.nAction) for i in range(2)]#下标1的网络是目标网络
+		self.experiments=set()
+
+
+	def GetValues(self,observation,index=0):
+		return self.models[index].predict(observation.reshape((1,self.nLen))).reshape((self.nAction,))
+
+	def GetAction(self,observation,ebsilon,index=0):
+		if random.random()<ebsilon:
+			return random.randint(0,self.nAction-1)
+		values=self.GetValues(observation,index)
+		return np.argmax(values)
+
+	def Save(self,filepath):#保存网络权值，并更新目标网络
+		self.models[0].save_weights(filepath)
+		#self.models[1].load_weights(filepath)
+		#self.models[1].save(filepath2)
+
+	def Load(self,filepath):
+		try:
+			self.models[0].load_weights(filepath)
+			self.models[1].load_weights(filepath)
+		except Exception as e:
+			print('loading model weights failed:',repr(e))
+
+	def DDQN(self,env,ebsilon):
+		first=env.reset()
+		bingo=0
+		done=False
+		while not done:
+			action=self.GetAction(first,ebsilon,0)
+			second, reward, done, info = env.step(action)
+			if reward>0:
+				bingo+=reward
+			exp=(tuple(first),action,reward,tuple(second),done)
+			if not exp in self.experiments:
+				self.experiments.add(exp)
+			if len(self.experiments)>=BATCH_SIZE:
+				batch_x=[]
+				batch_y=[]
+				selected=random.sample(self.experiments,BATCH_SIZE)
+				for s1,a,r,s2,d in selected:
+					s1=np.array(s1)
+					s2=np.array(s2)
+					batch_x.append(s1)
+					ys=self.GetValues(s1,0)
+					if d:
+						ys[a]=r
+					else:
+						na=self.GetAction(s2,0.0,0)
+						nys=self.GetValues(s2,1)
+						ys[a]=r+GAMMA*nys[na]
+					batch_y.append(ys)
+				self.models[0].fit(np.array(batch_x),np.array(batch_y),batch_size=BATCH_SIZE,epochs=1,verbose=1)
+			first=second
+		self.models[1].set_weights(self.models[0].get_weights())
+		return bingo
+
+	
+def main():
+	EPOCH=30000
+	weight_file='cartpole.h5'
+	env=gym.make('CartPole-v1')
+	dqn=DDQN(env)
+	if len(sys.argv)>1:
+		EPOCH=int(sys.argv[1])
+	if len(sys.argv)>2:
+		dqn.Load(sys.argv[2])
+		weight_file=sys.argv[2]
+	else:
+		dqn.Save(weight_file)
+	if len(sys.argv)>3:
+		log_file=sys.argv[3]
+	maxb=0
+	allb=0
+	for i in range(EPOCH):
+		b=dqn.DDQN(env,0.01)
+		allb+=b
+		if b>maxb:
+			maxb=b
+			print('got progress[%d] at %d'%(b,i))
+		if (i%10)==9:
+			dqn.Save(weight_file)
+			os.system("cls")
+			print('epoch %d finished!max progress:%d,average:%f'%(i,maxb,allb/10))
+			if log_file!=None:
+				with open(log_file,'a') as fp:
+					fp.write('epoch %d,max progress:%d,average:%f\n'%(i,maxb,allb/10))
+			allb=0
+
+
+if __name__=='__main__':
+	main()
---
+++ CartPoleRL/CartPoleVPG.py
@@ -0,0 +1,111 @@
+import tensorflow as tf
+import numpy as np
+import gym
+import random
+import sys
+import os
+
+GAMMA=0.95
+
+class BaselineVPG(object):
+	def __init__(self,env):
+		self.nLen=len(env.reset())
+		self.nAction=env.action_space.n
+		self.nInput=self.nLen
+		self.policy_net=tf.keras.Sequential([tf.keras.layers.InputLayer(input_shape=(self.nInput,)),
+			tf.keras.layers.Dense(30,activation='relu'),
+			tf.keras.layers.Dense(30,activation='relu'),
+			tf.keras.layers.Dense(self.nAction,activation=tf.nn.softmax)])
+		self.state_net=tf.keras.Sequential([tf.keras.layers.InputLayer(input_shape=(self.nInput,)),
+			tf.keras.layers.Dense(30,activation='relu'),
+			tf.keras.layers.Dense(30,activation='relu'),
+			tf.keras.layers.Dense(1,activation=tf.keras.activations.linear)])
+
+		self.policy_net.compile(optimizer=tf.keras.optimizers.Adam(1e-3),loss=tf.keras.losses.categorical_crossentropy)
+		self.state_net.compile(optimizer=tf.keras.optimizers.Adam(1e-3),loss='mse')
+
+
+	def GetValue(self,observation):#获取状态价值
+		obs=observation.reshape(1,self.nInput)
+		values=self.state_net.predict(obs)
+		return values[0,0]
+
+	def GetAction(self,observation):#动作决策
+		obs=observation.reshape(1,self.nInput)
+		probs=self.policy_net.predict(obs)[0]
+		return np.random.choice(self.nAction,p=probs)
+
+	def Learn(self,env):
+		trace=[]
+		bingo=0
+		done=False
+		first=env.reset()
+		while not done:
+			action=self.GetAction(first)
+			second, reward, done, info=env.step(action)
+			if reward>0:
+				bingo+=reward
+			trace.append((first,action,reward,done))
+			first=second
+		all_rewards=0
+		states=[]
+		values=[]
+		aps=[]
+		for s1,a,r,done in trace[::-1]:
+			states.append(s1)
+			all_rewards=all_rewards*GAMMA+r
+			values.append([all_rewards,])
+			ap=np.zeros((self.nAction,))
+			ap[a]=(all_rewards-self.GetValue(s1))
+			aps.append(ap)
+		self.state_net.fit(np.array(states),np.array(values),batch_size=len(trace))
+		self.policy_net.fit(np.array(states),np.array(aps),batch_size=len(trace))
+		return bingo
+	
+	def Save(self,filename):
+		self.policy_net.save(filename+'-p.h5')
+		self.state_net.save(filename+'-s.h5')
+
+	def Load(self,filename):
+		try:
+			pn=tf.keras.models.load_model(filename+'-p.h5')
+			sn=tf.keras.models.load_model(filename+'-s.h5')
+			self.policy_net=pn
+			self.state_net=sn
+		except BaseException as e:
+			print('loading failed:',repr(e))
+	
+def main():
+	EPOCH=30000
+	weight_file='cartpole_vpg'
+	env=gym.make('CartPole-v1')
+	agent=BaselineVPG(env)
+	if len(sys.argv)>1:
+		EPOCH=int(sys.argv[1])
+	if len(sys.argv)>2:
+		agent.Load(sys.argv[2])
+		weight_file=sys.argv[2]
+	#else:
+		#agent.Save(weight_file)
+	if len(sys.argv)>3:
+		log_file=sys.argv[3]
+	maxb=0
+	allb=0
+	for i in range(EPOCH):
+		b=agent.Learn(env)
+		allb+=b
+		if b>maxb:
+			maxb=b
+			print('got progress[%d] at %d'%(b,i))
+		if (i%10)==9:
+			agent.Save(weight_file)
+			os.system("cls")
+			print('epoch %d finished!max progress:%d,average:%f'%(i,maxb,allb/10))
+			if log_file!=None:
+				with open(log_file,'a') as fp:
+					fp.write('epoch %d,max progress:%d,average:%f\n'%(i,maxb,allb/10))
+			allb=0
+
+
+if __name__=='__main__':
+	main()
