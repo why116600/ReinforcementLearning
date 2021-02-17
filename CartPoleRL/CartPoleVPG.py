8484801055ea208f9852b73a839d3f5e91334993
why116600 <wanghy438@163.com>
2021-02-17 23:11:06 +0800

    使用GradientTape改正蒙特卡罗策略梯度

--- CartPoleRL/CartPoleVPG.py
+++ CartPoleRL/CartPoleVPG.py
@@ -13,12 +13,12 @@ class BaselineVPG(object):
 		self.nAction=env.action_space.n
 		self.nInput=self.nLen
 		self.policy_net=tf.keras.Sequential([tf.keras.layers.InputLayer(input_shape=(self.nInput,)),
-			tf.keras.layers.Dense(30,activation='relu'),
-			tf.keras.layers.Dense(30,activation='relu'),
+			tf.keras.layers.Dense(40,activation='relu'),
+			tf.keras.layers.Dense(40,activation='relu'),
 			tf.keras.layers.Dense(self.nAction,activation=tf.nn.softmax)])
 		self.state_net=tf.keras.Sequential([tf.keras.layers.InputLayer(input_shape=(self.nInput,)),
-			tf.keras.layers.Dense(30,activation='relu'),
-			tf.keras.layers.Dense(30,activation='relu'),
+			tf.keras.layers.Dense(40,activation='relu'),
+			tf.keras.layers.Dense(40,activation='relu'),
 			tf.keras.layers.Dense(1,activation=tf.keras.activations.linear)])
 
 		self.policy_net.compile(optimizer=tf.keras.optimizers.Adam(1e-3),loss=tf.keras.losses.categorical_crossentropy)
@@ -40,26 +40,27 @@ class BaselineVPG(object):
 		bingo=0
 		done=False
 		first=env.reset()
+		account=1.0
 		while not done:
 			action=self.GetAction(first)
 			second, reward, done, info=env.step(action)
 			if reward>0:
 				bingo+=reward
-			trace.append((first,action,reward,done))
+			trace.append((first,action,reward,done,account))
+			account*=GAMMA
 			first=second
 		all_rewards=0
-		states=[]
-		values=[]
-		aps=[]
-		for s1,a,r,done in trace[::-1]:
-			states.append(s1)
+		for s1,a,r,done,account in trace[::-1]:
 			all_rewards=all_rewards*GAMMA+r
-			values.append([all_rewards,])
-			ap=np.zeros((self.nAction,))
-			ap[a]=(all_rewards-self.GetValue(s1))
-			aps.append(ap)
-		self.state_net.fit(np.array(states),np.array(values),batch_size=len(trace))
-		self.policy_net.fit(np.array(states),np.array(aps),batch_size=len(trace))
+			self.state_net.fit(s1[np.newaxis],np.ones((1,1))*all_rewards,verbose=0)
+			td_error=all_rewards-self.state_net.predict(s1[np.newaxis])
+			x_tensor = tf.convert_to_tensor(s1[np.newaxis],dtype=tf.float32)
+			with tf.GradientTape() as tape:
+				pi_tensor = self.policy_net(x_tensor)[0, a]
+				logpi_tensor = tf.math.log(tf.clip_by_value(pi_tensor,1e-6, 1.))
+				loss_tensor = -account * td_error * logpi_tensor
+			grad_tensors = tape.gradient(loss_tensor, self.policy_net.variables)
+			self.policy_net.optimizer.apply_gradients(zip(grad_tensors, self.policy_net.variables))
 		return bingo
 	
 	def Save(self,filename):
@@ -99,7 +100,7 @@ def main():
 			print('got progress[%d] at %d'%(b,i))
 		if (i%10)==9:
 			agent.Save(weight_file)
-			os.system("cls")
+			#os.system("cls")
 			print('epoch %d finished!max progress:%d,average:%f'%(i,maxb,allb/10))
 			if log_file!=None:
 				with open(log_file,'a') as fp:
